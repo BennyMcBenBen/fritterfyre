059809a6aa64bc8c6e0838a2ef822d27b640a711
Ben Zoller <benjamin.zoller@gmail.com>
2019-07-03 23:52:49 -0400

    Add stub for /pony/name

--- src/pony.js
+++ src/pony.js
@@ -3,8 +3,12 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/svg', function (req, res, next) {
-    res.setHeader('Content-Type', 'image/svg+xml');
-    res.sendFile(path.join(__dirname, '../public/node.svg'));
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.sendFile(path.join(__dirname, '../public/node.svg'));
+});
+
+router.get('/name', function (req, res, next) {
+  res.send('Fritterfyre')
 });
 
 module.exports = router;
