4c9ac35127a4eff0d2860322e02f0180bf8a58c8
Ben Zoller <benjamin.zoller@gmail.com>
2019-07-08 15:04:13 -0400

    Randomize more pony colors

--- src/pony.js
+++ src/pony.js
@@ -16,13 +16,38 @@ router.get('/name', function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
+  let randomColor = () => '#'+((1<<24)*Math.random()|0).toString(16);
+  let lightenDarkenColor = (col, amt) => {
+    let usePound = false;
+    if (col[0] === "#") {
+      col = col.slice(1);
+      usePound = true;
+    }
+    const num = parseInt(col,16);
+    let r = (num >> 16) + amt;
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+    let b = ((num >> 8) & 0x00FF) + amt;
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+    let g = (num & 0x0000FF) + amt;
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+  };
+  const body = randomColor();
+  const bodyDarker = lightenDarkenColor(body, -20);
+  const hair = randomColor();
+  const hairLighter = lightenDarkenColor(hair, 20);
   // TODO implement SVG download
   // TODO randomize name
-  // TODO change leg color too
   // TODO change cutie mark
   // TODO more pony images? unicorn? pegasus? etc.
   const svg = fs.readFileSync(path.join(__dirname, '../public/pony.svg'), 'utf8')
-      .replace(new RegExp('#F791F6', 'gi'), '#'+((1<<24)*Math.random()|0).toString(16));
+      .replace(new RegExp('#F791F6', 'gi'), body)
+      .replace(new RegExp('#C76CCD', 'gi'), bodyDarker)
+      .replace(new RegExp('#6D2AB7', 'gi'), hair)
+      .replace(new RegExp('#A85EE0', 'gi'), hairLighter);
   const html = `
     <!DOCTYPE html>
     <html lang="en">
