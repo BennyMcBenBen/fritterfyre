a722abe94f8dc1a9b04ef930a9dde851e526a1e9
Paul Gray <pfbgray@gmail.com>
2019-10-28 11:08:13 -0400

    Making ponies better formatted

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .use('/pony', pony)
+  .get('/', pony)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
